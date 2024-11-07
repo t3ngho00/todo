6942e416bef36a08e97d9ccf9b7fd3bab491e56a
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2024-11-07 15:50:29 +0200

    Add user authentication context and protected routes; implement error handling and task management

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./screens/Home";
+import Authentication, { AuthenticationMode } from "./screens/Authentication";
+import ErrorPage from "./screens/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UserProvider from "./context/UserProvider";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/signin",
+    element: <Authentication authenticationMode={AuthenticationMode.Login} />
+  },
+  {
+    path: "/signup",
+    element: <Authentication authenticationMode={AuthenticationMode.Register} />
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path: "*", 
+    element: <ErrorPage />
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
 
